2d6985df92a4d829d9ae96eb87fa27e76ed384d4
Rohit Rohilla <rohit25431@iiitd.ac.in>
2026-06-11 10:21:08 +0530

    Added saved model prediction system

--- src/predict.py
+++ src/predict.py
@@ -1,39 +1,15 @@
 import pickle
-import pandas as pd
 
-df = pd.read_csv("data/processed/cleaned_sms.csv")
+with open("models/spam_classifier.pkl", "rb") as file:
+    model = pickle.load(file)
 
-from sklearn.feature_extraction.text import CountVectorizer
-
-vectorizer = CountVectorizer()
-
-X = vectorizer.fit_transform(df["clean_message"])
-
-y = df["label"]
-
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
-)
-
-from sklearn.naive_bayes import MultinomialNB
-
-model = MultinomialNB()
-
-model.fit(X_train, y_train)
-with open("models/spam_classifier.pkl", "wb") as file:
-    pickle.dump(model, file)
-
-with open("models/vectorizer.pkl", "wb") as file:
-    pickle.dump(vectorizer, file)
+with open("models/vectorizer.pkl", "rb") as file:
+    vectorizer = pickle.load(file)
 
 message = input("Enter a message: ")
 
 message_features = vectorizer.transform([message])
+
 prediction = model.predict(message_features)
 
 print("Prediction:", prediction[0])
